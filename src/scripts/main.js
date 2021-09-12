69bb7f10cb819dfce8e61cb6c9b1fb9a9eb9330f
H3JILgaH4uk <h3jilgah4uk@gmail.com>
2021-09-12 12:16:47 +0500

    Added theme switcher

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,6 +3,7 @@
 import lazyImages from './modules/lazyImages';
 import documentReady from './helpers/documentReady';
 import * as checkDevice from './helpers/checkDevice';
+// import * as themeSwitcher from './components/themeSwitcher';
 // import * as aos from './modules/aos.settings';
 import * as header from './parts/header';
 import * as smoothScroll from './components/smoothScroll';
