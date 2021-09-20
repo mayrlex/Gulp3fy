38b1d5ed8286a3844938e94eb7846884b0c59e57
H3JILgaH4uk <h3jilgah4uk@gmail.com>
2021-09-20 17:08:16 +0500

    Added sass due to transition to gulp-sass v5

--- gulp/tasks/styles.js
+++ gulp/tasks/styles.js
@@ -1,5 +1,6 @@
 import gulp from 'gulp';
-import sass from 'gulp-sass';
+import compiler from 'sass';
+import gulpSass from 'gulp-sass';
 import plumber from 'gulp-plumber';
 import autoprefixer from 'gulp-autoprefixer';
 import gulpGroup from 'gulp-group-css-media-queries';
@@ -8,6 +9,8 @@ import rename from 'gulp-rename';
 import gulpif from 'gulp-if';
 import sassGlob from 'gulp-sass-glob';
 import path from '../config';
+
+const sass = gulpSass( compiler );
 
 const StylesBuild = () =>
 	gulp.src(path.styles.src, { sourcemaps: path.isDev })
