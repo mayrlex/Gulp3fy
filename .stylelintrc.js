1355c3e05329f1fdeffb74437f3c976255d003a2
H3JILgaH4uk <h3jilgah4uk@gmail.com>
2021-09-20 16:48:30 +0500

    Updated .stylelintrc.js

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -1,9 +1,18 @@
 module.exports = {
 	extends: ['stylelint-config-standard', 'stylelint-config-rational-order', 'stylelint-prettier/recommended'],
-
-	plugins: ['stylelint-scss', 'stylelint-order'],
-
+	plugins: ['stylelint-scss', 'stylelint-order', 'stylelint-config-rational-order/plugin'],
 	rules: {
+		// Property Order
+		'order/properties-order': [[], { severity: 'warning' }],
+		'plugin/rational-order': [
+			true,
+			{
+				'border-in-box-model': false,
+				'empty-line-between-groups': false,
+				severity: 'warning',
+			},
+		],
+
 		'plugin/rational-order': [true, { 'empty-line-between-groups': true }],
 		'at-rule-no-unknown': null,
 		'scss/at-rule-no-unknown': true,
@@ -19,5 +28,6 @@ module.exports = {
 		'no-eol-whitespace': null,
 		'value-list-max-empty-lines': null,
 		'color-hex-case': null,
+		'no-descending-specificity': null,
 	},
 };
