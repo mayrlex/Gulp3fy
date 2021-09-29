3f5b3a667f10b3c206df61013f8f73caa0da3106
H3JILgaH4uk <h3jilgah4uk@gmail.com>
2021-09-29 21:31:15 +0500

    CheckDevice.js and SmoothScroll.js moved to modules

--- src/scripts/helpers/checkDevice.js
+++ src/scripts/helpers/checkDevice.js
@@ -1,42 +1,40 @@
-// Platforms
-const isMobile = {
-	Android: () => navigator.userAgent.match(/Android/i),
-	BlackBerry: () => navigator.userAgent.match(/BlackBerry/i),
-	iOS: () => navigator.userAgent.match(/iPhone|iPad|iPod/i),
-	Opera: () => navigator.userAgent.match(/Opera/i),
-	Windows: () => navigator.userAgent.match(/IEMobile/i),
+export default () => {
+	// Platforms
+	const isMobile = {
+		Android: () => navigator.userAgent.match(/Android/i),
+		BlackBerry: () => navigator.userAgent.match(/BlackBerry/i),
+		iOS: () => navigator.userAgent.match(/iPhone|iPad|iPod/i),
+		Opera: () => navigator.userAgent.match(/Opera/i),
+		Windows: () => navigator.userAgent.match(/IEMobile/i),
 
-	any: () =>
-		isMobile.Android() ||
-		isMobile.BlackBerry() ||
-		isMobile.iOS() ||
-		isMobile.Opera() ||
-		isMobile.Windows(),
+		any: () =>
+			isMobile.Android() || isMobile.BlackBerry() || isMobile.iOS() || isMobile.Opera() || isMobile.Windows(),
+	};
+
+	// Check platform
+	if (isMobile.any()) {
+		document.body.classList.add('--mob');
+
+		// const menuArrows = document.querySelectorAll('.menu__arrow');
+		// if (menuArrows.length > 0) {
+		// 	for (let index = 0; index < menuArrows.length; index++) {
+		// 		const menuArrow = menuArrows[index];
+		// 		menuArrow.addEventListener('click', (e) => {
+		// 			menuArrow.parrentElements.classList.toggle('active');
+		// 		});
+		// 	}
+		// }
+	} else {
+		document.body.classList.add('--pc');
+
+		// const menuArrows = document.querySelectorAll('.arrow');
+		// if (menuArrows.length > 0) {
+		// 	for (let index = 0; index < menuArrows.length; index++) {
+		// 		const menuArrow = menuArrows[index];
+		// 		menuArrow.addEventListener('click', (e) => {
+		// 			menuArrow.parrentElements.classList.toggle('active');
+		// 		});
+		// 	}
+		// }
+	}
 };
-
-// Check platform
-if (isMobile.any()) {
-	document.body.classList.add('--mob');
-
-	// const menuArrows = document.querySelectorAll('.menu__arrow');
-	// if (menuArrows.length > 0) {
-	// 	for (let index = 0; index < menuArrows.length; index++) {
-	// 		const menuArrow = menuArrows[index];
-	// 		menuArrow.addEventListener('click', (e) => {
-	// 			menuArrow.parrentElements.classList.toggle('active');
-	// 		});
-	// 	}
-	// }
-} else {
-	document.body.classList.add('--pc');
-
-	// const menuArrows = document.querySelectorAll('.arrow');
-	// if (menuArrows.length > 0) {
-	// 	for (let index = 0; index < menuArrows.length; index++) {
-	// 		const menuArrow = menuArrows[index];
-	// 		menuArrow.addEventListener('click', (e) => {
-	// 			menuArrow.parrentElements.classList.toggle('active');
-	// 		});
-	// 	}
-	// }
-}
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,12 +1,14 @@
 import lazyImages from './helpers/lazyImages';
 import documentReady from './helpers/documentReady';
+// import checkDevice from './helpers/checkDevice';
 // import * as themeSwitcher from '../components/themeSwitcher/themeSwitcher';
 import scrollBar from 'overlayscrollbars';
 import scrollBarRc from './config/scrollbarsrc';
 // import * as header from './parts/header';
-// import * as smoothScroll from './components/smoothScroll';
+// import * as smoothScroll from './modules/smoothScroll';
 import * as swiperRc from './config/swiperrc';
 
 documentReady(() => {
 	lazyImages();
+	// checkDevice();
 });
