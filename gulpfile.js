3bdc94d3b0caf8b54256d0e6f22e2c27b1aa8401
H3JILgaH4uk <H3JILgaH4uk@gmail.com>
2021-12-31 14:13:02 +0500

    Gulp has been rebuild

---
+++ gulpfile.js
@@ -0,0 +1,19 @@
+import gulp from 'gulp';
+import clean from './gulp/tasks/clean.js';
+import server from './gulp/tasks/server.js';
+import ftp from './gulp/tasks/ftp.js';
+import zip from './gulp/tasks/zip.js';
+import spritesBuild from './gulp/tasks/sprites.js';
+import { builds, watchers } from './gulp/config/tasks.js';
+
+const dev = gulp.series(clean, builds, gulp.parallel(watchers, server));
+const prod = gulp.series(clean, builds);
+// const archiving = gulp.series(clean, mainTasks, zip);
+const deploy = gulp.series(clean, builds, ftp);
+
+export { spritesBuild };
+
+export { dev };
+export { prod };
+export { zip };
+export { deploy };
