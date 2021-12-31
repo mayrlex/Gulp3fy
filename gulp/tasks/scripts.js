bb36f46d151db3a19d8a7e2a687ff0e62eb67da1
H3JILgaH4uk <H3JILgaH4uk@gmail.com>
2021-12-31 14:22:12 +0500

    Javascript is now compiled using webpack

---
+++ gulp/tasks/scripts.js
@@ -0,0 +1,36 @@
+import webpack from 'webpack-stream';
+import gulp from 'gulp';
+import plumber from 'gulp-plumber';
+import notify from 'gulp-notify';
+import sync from 'browser-sync';
+import { path } from '../config/path.js';
+
+const scripts = [path.scripts.watch, path.components.scripts];
+
+export const scriptsBuild = () => {
+	return gulp
+		.src(path.scripts.src, { sourcemaps: path.isDev })
+
+		.pipe(
+			plumber(
+				notify.onError({
+					title: 'JS',
+					message: 'Error: <%= error.message %>',
+				})
+			)
+		)
+
+		.pipe(
+			webpack({
+				mode: path.isProd ? 'production' : 'development',
+				output: {
+					filename: 'app.min.js',
+				},
+			})
+		)
+
+		.pipe(gulp.dest(path.scripts.dest))
+		.pipe(sync.stream());
+};
+
+export const scriptsWatch = () => gulp.watch(scripts, scriptsBuild);
