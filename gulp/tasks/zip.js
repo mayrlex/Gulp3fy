698734d675b5873d6aa1433e0237d862a1ddc594
H3JILgaH4uk <H3JILgaH4uk@gmail.com>
2021-12-31 14:35:33 +0500

    Added new task zip.js. Archives the entire dist folder

---
+++ gulp/tasks/zip.js
@@ -0,0 +1,27 @@
+import gulp from 'gulp';
+import plumber from 'gulp-plumber';
+import notify from 'gulp-notify';
+import del from 'del';
+import gzip from 'gulp-zip';
+import { path } from '../config/path.js';
+
+const zip = () => {
+	del(path.zip.del);
+
+	return gulp
+		.src(path.compiled, {})
+		.pipe(
+			plumber(
+				notify.onError({
+					title: 'ZIP',
+					message: 'Error: <%= error.message %>',
+				})
+			)
+		)
+
+		.pipe(gzip(path.zip.root))
+
+		.pipe(gulp.dest('./'));
+};
+
+export default zip;
