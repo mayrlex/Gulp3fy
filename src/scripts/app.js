8b544551fda8cd5a38830c4bba0f07944da282c2
H3JILgaH4uk <H3JILgaH4uk@gmail.com>
2022-01-06 16:53:18 +0500

    Footer and Header moved to components

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -3,7 +3,7 @@ import documentReady from './helpers/documentReady.js';
 // import checkDevice from './helpers/checkDevice';
 // import * as themeSwitcher from '../components/themeSwitcher/themeSwitcher';
 // import * as scrollbars from './modules/scrollbars.js';
-// import * as header from './parts/header';
+// import * as header from '../components/header/header.js';
 // import * as smoothScroll from './modules/smoothScroll';
 // import * as swiper from './modules/swiper.js';
 import * as homepage from './pages/home/main.js';
