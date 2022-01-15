4f13991b8be1ad3469255394fc5b9094f7b7b9b1
H3JILgaH4uk <H3JILgaH4uk@gmail.com>
2022-01-15 18:22:55 +0500

    Minor changes

--- src/scripts/modules/showHideAnimation.js
+++ src/scripts/modules/showHideAnimation.js
@@ -1,64 +1,63 @@
 const showAnim = (target, duration = 500) => {
-	const t = target;
 	if (!target.classList.contains('--slide')) {
-		t.classList.add('--slide');
+		target.classList.add('--slide');
 
 		if (target.hidden) {
-			t.hidden = false;
+			target.hidden = false;
 		}
 
 		const height = target.offsetHeight;
 
-		t.style.overflow = 'hidden';
-		t.style.height = 0;
-		t.style.paddingTop = 0;
-		t.style.paddingBottom = 0;
-		t.style.marginTop = 0;
-		t.style.marginBottom = 0;
-		t.offsetHeight;
-		t.style.transitionProperty = 'height, margin, padding';
-		t.style.transitionDuration = `${duration}ms`;
-		t.style.height = `${height}px`;
-		t.style.removeProperty('padding-top');
-		t.style.removeProperty('padding-bottom');
-		t.style.removeProperty('margin-top');
-		t.style.removeProperty('margin-bottom');
+		target.style.overflow = 'hidden';
+		target.style.height = 0;
+		target.style.paddingTop = 0;
+		target.style.paddingBottom = 0;
+		target.style.marginTop = 0;
+		target.style.marginBottom = 0;
+		target.offsetHeight;
+		target.style.transitionProperty = 'height, margin, padding';
+		target.style.transitionDuration = `${duration}ms`;
+		target.style.height = `${height}px`;
+		target.style.removeProperty('padding-top');
+		target.style.removeProperty('padding-bottom');
+		target.style.removeProperty('margin-top');
+		target.style.removeProperty('margin-bottom');
 
 		window.setTimeout(() => {
-			t.style.removeProperty('height');
-			t.style.removeProperty('overflow');
-			t.style.removeProperty('transition-duration');
-			t.style.removeProperty('transition-property');
-			t.classList.remove('--slide');
+			target.style.removeProperty('height');
+			target.style.removeProperty('overflow');
+			target.style.removeProperty('transition-duration');
+			target.style.removeProperty('transition-property');
+			target.classList.remove('--slide');
 		}, duration);
 	}
 };
 
 const hideAnim = (target, duration = 500) => {
-	const t = target;
 	if (!target.classList.contains('--slide')) {
-		t.classList.add('--slide');
-		t.style.transitionProperty = 'height, margin, padding';
-		t.style.transitionDuration = `${duration}ms`;
-		t.style.height = `${t.offsetHeight}px`;
-		t.offsetHeight;
-		t.style.overflow = 'hidden';
-		t.style.height = 0;
-		t.style.paddingTop = 0;
-		t.style.paddingBottom = 0;
-		t.style.marginTop = 0;
-		t.style.marginBottom = 0;
+		target.classList.add('--slide');
+		target.style.transitionProperty = 'height, margin, padding';
+		target.style.transitionDuration = `${duration}ms`;
+		target.style.height = `${target.offsetHeight}px`;
+		target.offsetHeight;
+		target.style.overflow = 'hidden';
+		target.style.height = 0;
+		target.style.paddingTop = 0;
+		target.style.paddingBottom = 0;
+		target.style.marginTop = 0;
+		target.style.marginBottom = 0;
+
 		window.setTimeout(() => {
-			t.hidden = true;
-			t.style.removeProperty('height');
-			t.style.removeProperty('padding-top');
-			t.style.removeProperty('padding-bottom');
-			t.style.removeProperty('margin-top');
-			t.style.removeProperty('margin-bottom');
-			t.style.removeProperty('overflow');
-			t.style.removeProperty('transition-duration');
-			t.style.removeProperty('transition-property');
-			t.classList.remove('--slide');
+			target.hidden = true;
+			target.style.removeProperty('height');
+			target.style.removeProperty('padding-top');
+			target.style.removeProperty('padding-bottom');
+			target.style.removeProperty('margin-top');
+			target.style.removeProperty('margin-bottom');
+			target.style.removeProperty('overflow');
+			target.style.removeProperty('transition-duration');
+			target.style.removeProperty('transition-property');
+			target.classList.remove('--slide');
 		}, duration);
 	}
 };
