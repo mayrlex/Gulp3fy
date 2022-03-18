ba2841a53b1bc4a13fff77fa0976497aee6079d8
H3JILgaH4uk <H3JILgaH4uk@gmail.com>
2022-03-18 15:37:27 +0500

    header.js updated

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -2,26 +2,12 @@ import burger from './burger.js';
 import Toggle from '../../scripts/modules/toggle.js';
 // import smoothScroll from '../../scripts/modules/smoothScroll.js';
 
-if (document.querySelector('header')) {
-	//? ### Modules
-	// smoothScroll();
-	burger();
+//? ### Modules
+// smoothScroll();
+burger();
 
-	// Dropdown list
-	if (document.querySelectorAll('.menu__item[data-dropdown]').length) {
-		const disabledLinks = document.querySelectorAll('.menu__sub-link.--disabled');
-
-		// Dropdown toggle
-		const dropdown = new Toggle({
-			btn: '[data-dropdown-btn]',
-			area: '[data-dropdown]',
-		});
-
-		// If there is a disabled item
-		if (disabledLinks.length) {
-			disabledLinks.forEach((link) => {
-				link.setAttribute('tabindex', '-1');
-			});
-		}
-	}
-}
+// Dropdown menu
+// const headerDropdown = new Toggle({
+// 	btn: '[data-dropdown-btn]',
+// 	area: '[data-dropdown]',
+// });
