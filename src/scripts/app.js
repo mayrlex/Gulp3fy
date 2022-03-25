3ed1ef0ba563463f8192166a3248593e7123161f
H3JILgaH4uk <H3JILgaH4uk@gmail.com>
2022-03-25 12:46:52 +0500

    Swiper moved to plugins folder

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -4,6 +4,9 @@ import documentReady from './helpers/documentReady.js';
 // import checkDevice from './helpers/checkDevice.js';
 import * as focusVisible from './helpers/focusVisible.js';
 
+//? ### Plugins
+// import Swiper, { Navigation, Pagination, Keyboard } from 'swiper';
+
 //? ### Components
 // import * as header from '../components/header/header.js';
 // import * as slider from '../components/slider/slider.js';
---
+++ src/scripts/plugins/swiper.js
@@ -0,0 +1,54 @@
+//? ### Template
+// const swiper = new Swiper('.swiper', {
+// 	modules: [Navigation, Pagination, Keyboard],
+
+// 	pagination: {
+// 		el: '.swiper-pagination',
+// 		clickable: true,
+// 	},
+
+// 	navigation: {
+// 		// nextEl: '.swiper-button-next',
+// 		// prevEl: '.swiper-button-prev',
+// 	},
+
+// 	keyboard: {
+// 		enable: true,
+// 		onlyInViewport: true,
+// 		pageUpDown: true,
+// 	},
+
+// 	mousewheel: {
+// 		sensivity: 1,
+// 	},
+
+// 	// direction: 'vertical',
+// 	// loop: true,
+// 	rewind: true,
+// 	speed: 800,
+// 	// autoplay: {
+// 	// 	delay: 5000,
+// 	// },
+
+// 	breakpoints: {
+// 		576: {
+// 			slidesPerView: 1,
+// 			spaceBetween: 10,
+// 		},
+
+// 		768: {
+// 			slidesPerView: 2,
+// 			spaceBetween: 20,
+// 		},
+
+// 		992: {
+// 			slidesPerView: 3,
+// 			spaceBetween: 20,
+// 		},
+
+// 		1400: {
+// 			slidesPerView: 4,
+// 			spaceBetween: 20,
+// 		},
+// 	},
+// });
