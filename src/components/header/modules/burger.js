460d81f2cf30bdfccfc8490ffad818b3ef0a8ee7
H3JILgaH4uk <H3JILgaH4uk@gmail.com>
2022-04-19 19:24:33 +0500

    Module "lock" is implemented in burger menu

--- src/components/header/modules/burger.js
+++ src/components/header/modules/burger.js
@@ -1,14 +1,18 @@
+import Lock from '../../../scripts/modules/lock.js';
+
 const burger = () => {
 	const backdrop = document.querySelector('.header__inner');
 	const menu = document.querySelector('.header__nav');
 	const target = document.querySelector('.burger');
+	const lock = new Lock({});
 
 	if (target) {
 		target.addEventListener('click', () => {
-			document.body.classList.toggle('--lock');
 			backdrop.classList.toggle('--show');
 			menu.classList.toggle('--show');
 			target.classList.toggle('--show');
+			target.classList.contains('--show') ? lock.lock() : null;
+			!target.classList.contains('--show') ? lock.unlock() : null;
 		});
 	}
 };
