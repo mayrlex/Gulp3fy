e1e09b1a8fbd47aae6ad1782d3b3c326adab67ad
H3JILgaH4uk <H3JILgaH4uk@gmail.com>
2022-04-26 15:28:58 +0500

    Minor changes/fixes

--- src/scripts/core/components.js
+++ src/scripts/core/components.js
@@ -1,2 +1,7 @@
-// import '../../components/header/header.js';
+import '../../components/header/header.js';
 // import '../../components/spoiler/spoiler.js';
+
+//? Modal
+// import Modal from '../../components/modal/modal.js';
+
+// const modal1 = new Modal({ target: 'modal-1' });
